2e9fa34ee55e80255ad495ff5082883b79eaa8b7
Ganesh-HA <haganesh2000@gmail.com>
2023-08-17 14:52:39 +0530

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,8 +24,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BranchComponent } from './branch/branch.component';
 import { StudentComponent } from './student/student.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'branches/:branchName', component: BranchComponent },
   { path: 'branches/:branchName/student/:studentId', component: StudentComponent }
   // Add more routes as needed
